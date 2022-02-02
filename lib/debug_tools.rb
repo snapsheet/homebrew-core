82ff4b38bd5f9e6c8a2a5df43aa8cd381fd589c5
d10zero <deena.tenzer@snapsheet.me>
2022-02-02 13:44:14 -0600

    SRE_1080 removing require in setup tools

--- lib/debug_tools.rb
+++ lib/debug_tools.rb
@@ -2,6 +2,5 @@ module DebugTools
   def setup_debug_tools
     Homebrew.install_gem_setup_path! 'pry-byebug', executable: 'pry'
     Homebrew.install_gem_setup_path! 'pry-rescue', executable: 'pry'
-    require 'pry-byebug'
   end
 end
