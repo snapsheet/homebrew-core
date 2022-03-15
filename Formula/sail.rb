1f0d02a474b50491a41e278e3270004349f9aaeb
Ray Olivero <ray.olivero@snapsheet.me>
2022-03-15 13:38:00 -0500

    Correct Homepage.

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -3,7 +3,7 @@ require_relative "../lib/sail/private_strategy"
 
 class Sail < Formula
   desc "Lighting-fast cli middleware for Docker containers"
-  homepage "https://github.com/raysn0w/sail"
+  homepage "https://github.com/bodyshopbidsdotcom/sail"
   url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.0/sail-1.0.0.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "50f22d0c6391ef18ecfa782a1316ba43df99c4fcfb079b4d65a328af569b95c1"
   license "MIT"
