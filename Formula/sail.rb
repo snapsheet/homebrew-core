0ca38ac57021154632ef22a62dc8833936a4a803
Ray Olivero <ray.olivero@snapsheet.me>
2022-03-16 09:17:24 -0500

    Patch v1.0.3

--- Formula/sail.rb
+++ Formula/sail.rb
@@ -4,8 +4,8 @@ require_relative "../lib/sail/private_strategy"
 class Sail < Formula
   desc "Lighting-fast cli middleware for Docker containers"
   homepage "https://github.com/bodyshopbidsdotcom/sail"
-  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.2/sail-1.0.2.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "81efa1ef7815a6fb9bd7e93df63cc193e6c4024ab334cfabfcd6a3b66b74302b"
+  url "https://github.com/bodyshopbidsdotcom/sail/releases/download/v1.0.3/sail-1.0.3.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "c9700eef7c43c8c15705ab32e178ee6996a0a1e45154f3c4484bb23060aea564"
   license "MIT"
 
   depends_on "jq"
