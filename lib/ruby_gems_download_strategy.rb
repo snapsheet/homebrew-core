47b902d1e89a42878a52b8e0f7212405eaa8990b
Rohin Paikattil <paikattilrohin@gmail.com>
2023-06-06 17:09:54 -0400

    Solution 2: Remove unused functions

--- lib/ruby_gems_download_strategy.rb
+++ lib/ruby_gems_download_strategy.rb
@@ -39,15 +39,7 @@ class RubyGemsDownloadStrategy < AbstractDownloadStrategy
   def clear_cache
     cached_location.unlink if cached_location.exist?
   end
-
-  def run_as_user(command_string)
-    system(*shell_cmd(command_string))
-  end
-
-  def shell_cmd(command_string)
-    [ENV['SHELL'], '-c', command_string]
-  end
-
+  
   def fetch(quiet: nil, verify_download_integrity: true, timeout: nil)
     ohai("Fetching #{name} gem from GitHub packages.")
     setup_debug_tools if Context.current.debug?
@@ -55,7 +47,7 @@ class RubyGemsDownloadStrategy < AbstractDownloadStrategy
     clear_cache
     RubyGemsDownloadStrategy.gem_config_file do |config_path|
       HOMEBREW_CACHE.cd do
-        run_as_user("gem fetch #{name} --version #{version} --config-file #{config_path}")
+        system("gem fetch #{name} --version #{version} --config-file #{config_path}")
       end
     end
   end
